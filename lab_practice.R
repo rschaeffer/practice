a89e7a2b215ce5dccd84220e4f3308e555393dd3
Bailey Crowley <60710757+BaileyLC@users.noreply.github.com>
2023-10-31 15:19:17 -0600

    Update lab_practice.R

--- lab_practice.R
+++ lab_practice.R
@@ -8,4 +8,12 @@
 # Create a data frame
   df <- data.frame("A" = c(1:10),
                    "B" = c(11:20))
-  
+
+# Check to see if it worked
+  df
+
+# Add column to df
+  df$C <- df$C = c(21:30)
+
+# Check to see if it worked
+  df
