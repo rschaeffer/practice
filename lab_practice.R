960ee7249f3eeaaa7cf1ec1b441cca14281c8458
robert schaeffer <schaeffer.robert@gmail.com>
2023-10-31 15:03:43 -0600

    Add files via upload

---
+++ lab_practice.R
@@ -0,0 +1,11 @@
+## Schaeffer Lab Practice
+
+# Prepare workspace ----
+
+# Set working directory
+  setwd("~/Downloads")
+
+# Create a data frame
+  df <- data.frame("A" = c(1:10),
+                   "B" = c(11:20))
+  
